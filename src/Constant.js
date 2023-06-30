9666b0549ec82b355c7ecc1752dbb59e48a14fe7
satnamskhowal <satnam.singh@synectiks.com>
2023-06-30 17:22:00 +0530

    fresh

--- src/Constant.js
+++
@@ -1,6 +0,0 @@
-
-export const constantErr = {
-    LOGIN_USER_NAME: "User is required",
-    LOGIN_USER_PASSWORD: "Password is required"
-
-}
--- src/config.js
+++
@@ -1,9 +0,0 @@
-// const IP = "localhost";
-const IP = "datawarehouse.mayanksoftwares.com";
-
-export default {
-    basePath: "/",
-    // apiUrl: "http://localhost:7050/api",
-    apiUrl: `https://${IP}/api`,
-    enableGA: true
-};
